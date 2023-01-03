474c691696176fe0497a648f00b94e3ff104e894
Ntsay Zacarias <ntsayyzacarias@gmail.com>
2023-01-03 21:16:54 +0000

    A few changes

--- src/Graph.h
+++ src/Graph.h
@@ -19,22 +19,27 @@
 // Represents a Graph of airports and flights
 class Graph {
 public:
-    void addFlight(const Flight &flight);
-    void addAirport(const Airport &airport);
-    std::vector<Airport> GetAirportsWithinDistance(double latitude, double longitude, double max_distance) const;
-    int getNumFlightsFromAirport(const Airport &airport) const;
-    std::vector<std::vector<Flight>>  FindShortestRoutes(const Airport &departure, const Airport &arrival) const;
-    std::vector<Flight> FindShortestRoute(const Airport &departure, const Airport &arrival) const;
-    //std::vector<std::vector<Flight>> GetFlightsFromAirportToAirport(const Airport &departure, const Airport &arrival) const;
-    std::vector<std::vector<Flight>> GetPathsWithOneAirline(const Airport &departure, const Airport &arrival, const std::string &airline) const;
+/*!
+ * 
+ * @param flight
+ */
+void addFlight(const Flight &flight);
+
+void addAirport(const Airport &airport);
+std::vector<Airport> GetAirportsWithinDistance(double latitude, double longitude, double max_distance) const;
+int getNumFlightsFromAirport(const Airport &airport) const;
+std::vector<std::vector<Flight>>  FindShortestRoutes(const Airport &departure, const Airport &arrival) const;
+std::vector<Flight> FindShortestRoute(const Airport &departure, const Airport &arrival) const;
+//std::vector<std::vector<Flight>> GetFlightsFromAirportToAirport(const Airport &departure, const Airport &arrival) const;
+std::vector<std::vector<Flight>> GetPathsWithOneAirline(const Airport &departure, const Airport &arrival, const std::string &airline) const;
 
 private:
-    // Adjacency list representation of the graph
-    std::unordered_map<std::string, std::vector<Flight>> adjacency_list;
-    // Map from airport names to airport objects
-    std::unordered_map<std::string, Airport> airports;
+// Adjacency list representation of the graph
+std::unordered_map<std::string, std::vector<Flight>> adjacency_list;
+// Map from airport names to airport objects
+std::unordered_map<std::string, Airport> airports;
 
-    double getDistance(const Airport &airport1, const Airport &airport2) const;
+double getDistance(const Airport &airport1, const Airport &airport2) const;
 };
 
 
--- src/Manager.h
+++ src/Manager.h
@@ -24,26 +24,54 @@ private:
     std::vector<Airport> departuresAirports,arrivalAirports;
 public:
     Manager()= default;
+    ///
     void run();
     void testing();
+    ///
+    /// \param fname1
+    /// \param fname2
+    /// \param fname3
+    /// \return true if the vectors size is equivalent to the number of lines in the files
     bool loadData(const std::string& fname1, const std::string& fname2, const std::string& fname3);
-
+    ///
     void main_menu();
-
+    ///
+    /// \param notARecursiveCall
     void search_flights_menu(bool notARecursiveCall);
-
+    ///
+    /// \param departures_airports
+    /// \param arrival_airports
     void search_flights(std::vector<Airport> departures_airports, std::vector<Airport> arrival_airports);
-
+    ///
+    /// \return list of Airports
     std::vector<Airport> getAirportsCityCountry();
-
+    ///
+    /// \return
     Airport getAirportByCode();
-
+    ///
+    /// \return
     std::vector<Airport> getAirportsByCoordinates();
-
-
+    ///
+    /// \param departures_airports
+    /// \param arrival_airports
+    /// \param route
     void show_flight_info(const std::vector<Airport>& departures_airports, const std::vector<Airport>& arrival_airports,
                           const std::vector<Flight>& route);
 };
 
 
 #endif //FEUP_AED2_MANAGER_H
+
+
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
